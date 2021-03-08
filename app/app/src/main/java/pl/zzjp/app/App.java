7215633e4c70398c374ea99991d9ea9d7d806793
bruderooo <szymonjacon@gmail.com>
2021-03-08 23:32:55 +0100

    Add properties.

--- app/app/src/main/java/pl/zzjp/app/App.java
+++ app/app/src/main/java/pl/zzjp/app/App.java
@@ -1,12 +1,30 @@
 package pl.zzjp.app;
 
-import pl.zzpj.lib.Person;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class App {
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("Szymon");
-        person.welcome();
-        System.out.println();
+        loadProperties();
+    }
+
+    public static Properties loadProperties() {
+        App app = new App();
+        Properties prop = app.loadPropertiesFile("config.properties");
+        prop.forEach((k, v) -> System.out.println(k + ":" + v));
+        return prop;
+    }
+
+    public Properties loadPropertiesFile(String filePath) {
+
+        Properties prop = new Properties();
+
+        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(filePath)) {
+            prop.load(resourceAsStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + filePath);
+        }
+        return prop;
     }
 }
